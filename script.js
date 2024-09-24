36cbdf449cf2c4fd4e3788b485c89d429100803c
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-24 12:29:00 -0400

    check all

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 
         function show() {
-        let y = document.getElementById("Geometry Intensified").value;
+        
+        let y = document.getElement("Geometry Intensified").value;
         document.getElementById('plz').textContent = y;
+        
         }
+         
+        function checkCheckboxes() {
+        
+        let checkboxes = document.querySelectorAll(".checkboxes");
+        
+        
+        let checkedValues = [];
+        
+        
+        checkboxes.forEach(function(checkbox) {
+          if (checkbox.checked) {
+            checkedValues.push(checkbox.value);  
+          }
+        });
+        
+        // Display the checked values
+        document.getElementById("selectedCheckboxes").textContent = "Selected: " + checkedValues.join(", ");
+      }
   
