f81b639a304476cd070564d02463942c2d575896
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-24 15:02:30 -0400

    revert plz work

--- script.js
+++ script.js
@@ -1,27 +1,26 @@
 
-        function show() {
+function show() {
         
-        let y = document.getElement("Geometry Intensified").value;
-        document.getElementById('plz').textContent = y;
-        
-        }
-         
-        function checkCheckboxes() {
-        
-        
-        let checkboxes = document.querySelectorAll("checkboxes");
-        
-        
-        let checkedValues = [];
-        
-        
-        checkboxes.forEach(function(checkbox) {
-          if (checkbox.checked) {
-            checkedValues.push(checkbox.value);  
-          }
-        });
-        
-        // Display the checked values
-        document.getElementById('plz').textContent = "Selected: " + checkedValues.join(", ");
-      }
+  let y = document.getElement("Geometry Intensified").value;
+  document.getElementById('plz').textContent = y;
   
+  }
+   
+  function checkCheckboxes() {
+  
+  
+  let checkboxes = document.querySelectorAll("checkboxes");
+  
+  
+  let checkedValues = [];
+  
+  
+  checkboxes.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      checkedValues.push(checkbox.value);  
+    }
+  });
+  
+  // Display the checked values
+  document.getElementById('plz').textContent = "Selected: " + checkedValues.join(", ");
+}
