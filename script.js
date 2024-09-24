8244c20eae3ce6d2114e7c1fbaeb5498ed03fef0
alexhamill <60296840+alexhamill@users.noreply.github.com>
2024-09-24 15:21:50 -0400

    test

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 
-   
-  function checkCheckboxes() {
-  
+function checkCheckboxes() {
   
   let checkboxes = document.querySelectorAll(".checkboxes");
   
   
   let checkedValues = [];
-  
-  
+
   checkboxes.forEach(function(checkbox) {
+    
     if (checkbox.checked) {
-      checkedValues.push(checkbox.value);  
+      checkedValues.push(checkbox.value); 
     }
   });
   
   // Display the checked values
-  document.getElementById("plz").textContent = "Selected: " + checkedValues.join(", ");
+  document.getElementById('plz').textContent = checkedValues.length > 0 ? 
+    "Selected: " + checkedValues.join(", ") : "No classes selected";
 }
